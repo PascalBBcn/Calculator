5d2263d2c67f7f4df0677061e3ca9c2448687581
Pascal <pascalbbcn@gmail.com>
2023-03-25 17:35:29 +0100

    add basic operation functions

--- calculator.js
+++ calculator.js
@@ -1,18 +1,37 @@
-let x = 0;
-let y = 0;
-let o = undefined;
-function add() {
-
+// let x = 0;
+// let y = 0;
+// let operator = undefined;
+function add(x, y) {
+    let output = x + y;
+    return output;
 }
 
-function subtract() {
-
+function subtract(x, y) {
+    let output = x - y;
+    return output;
 }
 
-function multiply() {
-
+function multiply(x, y) {
+    let output = x * y;
+    return output;
 }
 
-function divide() {
-    
+function divide(x, y) {
+    let output = x / y;
+    return output;
 }
+console.log(add(10, 5));
+console.log(subtract(10, 5));
+console.log(multiply(10, 5));
+console.log(divide(10, 5));
+// function operate(n1, o, n2) {
+//     if() {
+
+//     }
+//     else if() {
+
+//     }
+//     else if() {
+
+//     }
+// }
