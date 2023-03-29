aae631b7eae3ec7b09aa3cabfa74491efa1730a4
Pascal <pascalbbcn@gmail.com>
2023-03-29 12:20:16 +0200

    fix basic two number calculations. Allow display of output. Must still fix divide as it outputs undefined

--- calculator.js
+++ calculator.js
@@ -1,38 +1,76 @@
-// let num1 = undefined;
-// let num2 = undefined;
-// let operator = undefined;
+
 function add(x, y) {
     let output = x + y;
-    return output;
+    display.innerHTML = output;
 }
 
 function subtract(x, y) {
     let output = x - y;
-    return output;
+    display.innerHTML = output;
 }
 
 function multiply(x, y) {
     let output = x * y;
-    return output;
+    display.innerHTML = output;
 }
 
 function divide(x, y) {
     let output = x / y;
-    return output;
+    display.innerHTML = output;
 }
 
 function operate(n1, o, n2) {
-    if(o == 1) {
+    if(o == '+') {
         return add(n1, n2);
     }
-    else if(o == 2) {
+    else if(o == '-') {
         return subtract(n1, n2);
     }
-    else if(o == 3) {
+    else if(o == 'x') {
         return multiply(n1, n2);
     }
-    else if(o == 4) {
+    else if(o == '&divide') {
         return divide(n1, n2);
     }
 }
-console.log(operate(1, 4, 2));
+
+let display = document.getElementById("display");
+let displayArray = [];
+let numberArray1 = [];
+let numberArray2 = [];
+let operator = undefined;
+
+const button = document.querySelector('.equalsButton');
+  button.addEventListener('click', () => {
+    for(let i = 0; i < displayArray.length; i++)
+    {
+        if(displayArray[i] == 'x' || displayArray[i] == '&divide' || 
+           displayArray[i] == '+' || displayArray[i] == '-') {
+
+            operator = displayArray[i];
+
+            for(let j = 0; j < i; j++) {
+                numberArray1.push(displayArray[j]);
+            }
+            for(let k = i+1; k < displayArray.length; k++) {
+                numberArray2.push(displayArray[k]);
+            }
+            console.log(operator);
+        }
+    }
+    let num1 = parseInt(numberArray1.join(""));
+    let num2 = parseInt(numberArray2.join(""));
+    console.log(operate(num1, operator, num2));
+  });
+
+function populateDisplay(item) {
+    display.innerHTML = item;
+    displayArray.push(item);
+    display.innerHTML = displayArray.join("");
+    console.log(displayArray);
+    if(item == null) {
+        displayArray = [];
+        numberArray1 = [];
+        numberArray2 = [];
+    }
+}
